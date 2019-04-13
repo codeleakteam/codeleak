7cf62d7230a2caeb6a53bff1ac1ee8e44d84ae22
Bojan Milicev <boyan93@gmail.com>
2019-04-13 22:33:05 +0200

    Connect with api, start

--- django-backend/codeleak_backend/codeleak_backend/settings.py
+++ django-backend/codeleak_backend/codeleak_backend/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,7 +52,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    
+    'localhost:3000'
+)
+
 
 ROOT_URLCONF = 'codeleak_backend.urls'
 
---
+++ nextjs/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import getConfig from 'next/config'
+const { publicRuntimeConfig } = getConfig()
+
+const BASE_URL = publicRuntimeConfig.baseUrl
+
+export const apiGet = {
+  getIndex: () => {
+    return axios.get(`${BASE_URL}/api/home`)
+  },
+}
