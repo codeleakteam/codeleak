f80fae0497cc514dcd9164a631f79da03cd4f975
MilePaor <zlinindza@gmail.com>
2019-04-16 00:45:31 +0200

    Add background on landing

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -2,6 +2,9 @@ const withSass = require('@zeit/next-sass')
 const withCSS = require('@zeit/next-css')
 const withPlugins = require('next-compose-plugins')
 
+// Webpack config for images in css
+// https://whoisryosuke.com/blog/2018/nextjs-tip-using-media-in-css/
+
 const nextConfig = {
   publicRuntimeConfig: {
     baseUrl: 'http://localhost:8000',
@@ -12,6 +15,19 @@ module.exports = withPlugins(
   [
     withCSS(
       withSass({
+        webpack(config, options) {
+          config.module.rules.push({
+            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+            use: {
+              loader: 'url-loader',
+              options: {
+                limit: 100000,
+              },
+            },
+          })
+
+          return config
+        },
         cssModules: true,
         cssLoaderOptions: {
           localIdentName: '[local]___[hash:base64:5]',
