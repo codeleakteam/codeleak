197f5d5abf076aa5e12f02beba5075f8fff99d22
Branko Zivanovic <zivanovic.b1@hotmail.com>
2019-04-18 02:16:59 +0200

    Added gunicorn. Masked out admin url. Removed all unnecesary endpoints. Static root yet to be served.

--- django-backend/codeleak_backend/codeleak_backend/urls.py
+++ django-backend/codeleak_backend/codeleak_backend/urls.py
@@ -29,15 +29,15 @@ from core.views import (
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^api/users/(?P<user_id>[0-9]+)$', GetUpdateUserView.as_view()),
-    path('api/home', HomeView.as_view()),
+    path('lamerajlame/', admin.site.urls),
+    # url(r'^api/users/(?P<user_id>[0-9]+)$', GetUpdateUserView.as_view()),
+    # path('api/home', HomeView.as_view()),
     path('api/subscribe',CreateSubscriberView.as_view()),
-    path('api/questions', CreateQuestionView.as_view()),
-    url(r'^api/questions/(?P<question_id>[0-9]+)$', GetQuestionView.as_view()),
-    url(r'^api/questions/(?P<question_id>[0-9]+)$', UpdateQuestionView.as_view()),
-    url(r'^api/questions/(?P<question_id>[0-9]+)/vote$', UpdateQuestionScoreView.as_view()),
-    path('api/tags', ListCreateTagView.as_view()),
-    path('api/search', user_question_tag_search),
-    url(r'^api-auth/', include('rest_framework.urls'))
+    # path('api/questions', CreateQuestionView.as_view()),
+    # url(r'^api/questions/(?P<question_id>[0-9]+)$', GetQuestionView.as_view()),
+    # url(r'^api/questions/(?P<question_id>[0-9]+)$', UpdateQuestionView.as_view()),
+    # url(r'^api/questions/(?P<question_id>[0-9]+)/vote$', UpdateQuestionScoreView.as_view()),
+    # path('api/tags', ListCreateTagView.as_view()),
+    # path('api/search', user_question_tag_search),
+    # url(r'^api-auth/', include('rest_framework.urls'))
 ]
