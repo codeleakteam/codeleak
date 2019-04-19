138fa5cb83899274f0a2b1886c686932f8e0e42c
Bojan Milicev <boyan93@gmail.com>
2019-04-20 00:49:51 +0200

    Add icons

--- nextjs/pages/_document.js
+++ nextjs/pages/_document.js
@@ -1,12 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import getConfig from 'next/config'
-
 const { publicRuntimeConfig } = getConfig()
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     // Check if in production
-    // const isProduction = process.env.NODE_ENV === 'production'
     const isProduction = publicRuntimeConfig.baseUrl === 'http://codeleak.io' ? true : false
     const initialProps = await Document.getInitialProps(ctx)
     // Pass isProduction flag back through props
@@ -26,22 +24,39 @@ class MyDocument extends Document {
 
   render() {
     const { isProduction } = this.props
-
     return (
       <Html>
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {/* google font */}
           <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet" />
+          {/* antd */}
           <link href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.11.2/antd.css" rel="stylesheet" />
-
-          <link rel="icon" type="image/png" sizes="32x32" href="../static/favicons/favicon32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="../static/favicons/favicon16.png" />
+          {/* icons */}
+          <link rel="apple-touch-icon" sizes="57x57" href="../static/favicons/favicon57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="../static/favicons/favicon60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="../static/favicons/favicon72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="../static/favicons/favicon76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="../static/favicons/favicon114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="../static/favicons/favicon120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="../static/favicons/favicon144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="../static/favicons/favicon152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="../static/favicons/favicon180x180.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="../static/favicons/favicon192x192.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="../static/favicons/favicon32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="../static/favicons/favicon96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="../static/favicons/favicon16x16.png" />
+          <meta name="msapplication-TileImage" content="../static/favicons/favicon144x144.png" />
+          {/* metas */}
+          <meta
+            name="description"
+            content="An online-editor based question and answer platform for front-end developers"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
-
           {/* We only want to add the scripts if in production */}
           {isProduction && (
             <React.Fragment>
