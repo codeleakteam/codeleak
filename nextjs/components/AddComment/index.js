3becb41cf5b0ff234b6bd6bf4879c6de1a799874
Bojan Milicev <boyan93@gmail.com>
2019-05-20 22:53:21 +0200

    Add comment on question

--- nextjs/components/AddComment/index.js
+++ nextjs/components/AddComment/index.js
@@ -24,8 +24,6 @@ class AddComment extends Component {
     this.setState({ commentText: e.target.value })
   }
 
-  // submitComment = async
-
   render() {
     const { commentVisible } = this.state
     return (
@@ -36,7 +34,14 @@ class AddComment extends Component {
         {commentVisible && (
           <React.Fragment>
             <TextArea className={classes.comment__text} placeholder="Add comment" onChange={this.handleCommentText} />
-            <Button type="primary">Send</Button>
+            <Button
+              type="primary"
+              onClick={() =>
+                this.props.submitComment('QUESTION_COMMENT', this.props.questionId, 1, this.state.commentText)
+              }
+            >
+              Send
+            </Button>
           </React.Fragment>
         )}
       </div>
